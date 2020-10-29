7f055353d1385820e8d8363a7f0c877ee7f3e7e8
admin <admin@MacBook-Pro-admin.local>
2020-10-29 22:44:38 +0700

    initial commit

---
+++ api/http.go
@@ -0,0 +1,66 @@
+package api
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/pkg/errors"
+)
+
+type RedirectHandler interface {
+	Get(http.ResponseWriter, *http.Request)
+	Post(http.ResponseWriter, *http.Request)
+}
+
+type handler struct {
+	redirectService shortener.redirectService
+}
+
+func NewHandler(redirectService shortener.redirectService) RedirectHandler {
+	return &handler{redirectService: redirectService}
+}
+
+func setupResponse(w http.ResponseWriter, contentType string, body []byte, statusCode int) {
+	w.Header().Set("Content-Type", contentType)
+	w.WriteHeader(statusCode)
+	_, err := w.Write(body)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (h *handler) serializer(contentType string) shortener.RedirectSerializer {
+	if contentType == "application/x-msgpack" {
+		return &ms.Redirect{}
+	}
+	return &js.Redirect{}
+}
+
+func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
+	code := chi.URLParam(r, "code")
+	redirect, err := h.redirectService.Find(code)
+	if err != nil {
+		panic(err)
+	}
+	http.Redirect(w, r, redirect.URL, http.StatusMovedPermanently)
+}
+
+func (h *handler) Post(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("Content-Type")
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+	err = h.redirectService.Store(redirect)
+	if err != nil {
+		panic(err)
+	}
+	responseBody, err := h.serializer(contentType).Encode(redirect)
+	if err != nil {
+		panic(err)
+	}
+	setupResponse(w, contentType, responseBody, http.StatusCreated)
+}
